ca02f5436dd7f71b90ccb1aa913e0dee6e1f3d7d
jrrlucenafilho <jrrlucenafilho@gmail.com>
2023-12-08 15:43:14 -0300

    Added distMatrix as a param to CalcNodeInsertion func

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -7,7 +7,93 @@ using namespace std;
 typedef struct {
     vector<int> sequence;
     double cost;
-} tsp_solution;
+} TspSolution;
+
+struct InsertionInfo {
+    //Node that'll be inserted as graph gets built
+    int insertedNode;
+    //Edge that'll be removed as insertedNode gets added into the graph
+    //To be substituted by 2 other edges. Both of which connect one previously-interconnected
+    //node to to the new insertedNode 
+    int removedGraphEdge;
+    //Cost of inserting this new node into the graph, among tho other nodes
+    double cost;
+};
+
+/**
+ * @brief Calcs the cost of inserting the new node into the graph. (vertex and node used interchangaebly here)
+ * @param tspSol To-be-evaluated TSP Solution
+ * @param unaddedVertices Vertices / Nodes that still don't belong to the solution subtour
+ * @return insertionCost Cost of inserting nodes into TSP Solution graph
+ */
+vector<InsertionInfo> CalcNodeInsertionCost(TspSolution& tspSol, vector<int>& unaddedVertices, double** distMatrix)
+{
+    //Specifies size of vec
+    vector<InsertionInfo> insertionCost = vector<InsertionInfo>((tspSol.sequence.size() - 1) * unaddedVertices.size());
+    int l = 0;
+    int i, j;
+
+    //Iterating among all nodes in the solution sequence, going edge-by-edge (df 2 nodes on each edge)
+    for(int edgeIter = 0; edgeIter < (int)tspSol.sequence.size() - 1; edgeIter++){
+        i = tspSol.sequence[edgeIter];  //Vertex 1
+        j = tspSol.sequence[edgeIter + 1];  //Vertex 2
+
+        //For each node/vertex
+        for(int k : unaddedVertices){
+            insertionCost[l].cost = distMatrix[i][k] + distMatrix[j][k] - distMatrix[i][j];    //TODO: Check about c
+            insertionCost[l].insertedNode = k;
+            insertionCost[l].removedGraphEdge = edgeIter;
+            l++;
+        }
+    }
+
+    return insertionCost;
+}
+
+/**
+ * @brief Builds a fair solution (though still far from optimized) Using Greedy Randomized Adaptive Search (Insertion-by-cheapest)
+ * @return TspSolution
+ */
+TspSolution BuildSolution(double** distMatrix)
+{
+    
+}
+
+TspSolution IteratedLocalSearch(int maxIters, int maxIterILS, double** distMatrix)
+{
+    TspSolution bestOfAllSolution;
+    bestOfAllSolution.cost = INFINITY;
+
+    for(int i = 0; i < maxIters; i++){
+        //Builds a beginning solution based on fair guesses
+        TspSolution currIterSolution = BuildSolution(distMatrix);
+        TspSolution currBestSolution = currIterSolution;
+
+        int iterILS = 0;
+
+        while(iterILS <= maxIterILS){
+            //Tries to enhance the fair-guessed solution
+            //By doing small modifications to it
+            //LocalSearch(&currIterSolution);
+
+            if(currIterSolution.cost < currBestSolution.cost){
+                currBestSolution = currIterSolution;
+                iterILS = 0;
+            }
+
+            //If not possible to make it better, shake the current best solution up a lil
+            //to see if we didn't just go into a 'local best pitfall'
+            //currIterSolution = Disturbance(currBestSolution);
+            iterILS++;
+        }
+
+        if(currBestSolution.cost < bestOfAllSolution.cost){
+            bestOfAllSolution = currBestSolution;
+        }
+    }
+
+    return bestOfAllSolution;
+}
 
 int main(int argc, char** argv)
 {
@@ -21,7 +107,7 @@ int main(int argc, char** argv)
     data.printMatrixDist();
 
     //Heuristic goes here
-
+    IteratedLocalSearch(5, 5, data.getMatrixCost());
 
     cout << "----------------------\n";
     cout << "Exemplo de Solucao s = ";
